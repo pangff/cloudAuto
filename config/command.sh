f56f71adb321854fb9cfd62531fac9ec8ac9b138
“pangff” <admin@example.com>
2017-03-14 13:02:03 +0800

    add echo

--- config/command.sh
+++ config/command.sh
@@ -1,10 +1,10 @@
-echo "==install env==="
-cd ~/git
-git clone https://github.com/creationix/nvm.git
-source ~/git/nvm/nvm.sh
-NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node nvm install 7.5.0
-npm --registry=https://registry.npm.taobao.org install cnpm -g
-cd ..
+#echo "==install env==="
+#cd ~/git
+#git clone https://github.com/creationix/nvm.git
+#source ~/git/nvm/nvm.sh
+#NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node nvm install 7.5.0
+#npm --registry=https://registry.npm.taobao.org install cnpm -g
+#cd ..
 cd cloudAuto/simple-restify
 cnpm install;
 npm install pm2@latest -g
