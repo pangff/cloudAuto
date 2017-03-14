921182da724f5c4c01bb89eef71ca19bd78a3822
“pangff” <admin@example.com>
2017-03-14 13:21:10 +0800

    add echo

--- config/command.sh
+++ config/command.sh
@@ -5,9 +5,9 @@
 #NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node nvm install 7.5.0
 #npm --registry=https://registry.npm.taobao.org install cnpm -g
 #cd ..
-cd ~/code/cloudAuto/simple-restify
+#cd ~/code/cloudAuto/simple-restify
 #cnpm install;
 #npm install pm2@latest -g
 echo "==start server==="
-pm2 start index.js
+pm2 start ~/code/cloudAuto/simple-restify/index.js
 echo "==done==="
